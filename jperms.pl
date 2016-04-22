0fa5918843b10e71e01902dd6841fa5086059c2a
lqueryvg <lqueryvg@gmail.com>
2016-04-22 16:15:54 +0100

    mooooore

--- jperms.pl
+++ jperms.pl
@@ -7,41 +7,46 @@ use warnings;
 use File::Find;
 use Getopt::Long;
 use Pod::Usage;
+use IO::File;
+use POSIX;
+use File::stat;
 
 # Get command line options
 #
 my %options;
-GetOptions(\%options, 'help|?|h', 'man', 'verbose', 'dir=s', 'no_run');
+GetOptions(\%options, 'help|?|h', 'file=s',
+                      'man', 'verbose', 'dir=s', 'no_run');
 pod2usage(1) if $options{help};
-pod2usage(-exitval => 1, -verbose => 2) if $options{man};
+pod2usage(-verbose => 2) if $options{man};
+pod2usage({ -message => q{ERROR: -file is required} }) unless $options{file};
+pod2usage({ -message => q{ERROR: -dir is required} })  unless $options{dir};
 
+# Subroutines
+
+sub zero_pad_octal_string {
+  my ($str) = @_;
+  if ($str ne '-' && substr($str, 0, 1) ne '0') {
+    $str = '0' . $str;
+  }
+  #return oct($str);
+  return $str;
+}
 
 # Process pattern rules
 
-my $patterns_config = '
-  # pattern                      owner   group  dperm    fperm
-  #
-  \./test/data.*/proj/cards	     appuser grp2    770      -
-  \./test/data.*/proj/r/r_import appuser grp2    770      -
-  \./test/data.*/proj/outbound 	 appuser adm    2770      -
-  \./test/data.*/proj/.*/inbox 	 appuser grp2    770      -
-  \./test/data.*/proj/[^/]+      appuser grp2    750      -
-  \./test/data[^/]*/proj	     appuser adm     750      -
-  \./test/data.*/tws/logs 	     appuser grp2    770      -
-  \./test/data.*/tws 	         appuser grp2    750      -
-  \./test/data.*/logs 	         appuser grp2    770      -
-  \./test/data.*/scripts 	     root    adm     750      -
-  \./test/data.*                 appuser grp2    770      660
-  \./test/.*                     -       -  	 -        -
-  \./test	                     appuser appgrp  775      -
-';
+my $fh = IO::File->new($options{file}, q{<})
+  or die "ERROR: unable to open patterns file $options{file}\n";
+
 
 print "parsing pattern rules...\n" if $options{verbose};
 
 my %pattern_details = ();
 my @pattern_list = ();
 
-for my $line (split("\n", $patterns_config)) {
+
+while (my $line = <$fh>) {
+
+#for my $line (split("\n", $patterns_config)) {
 
   print "pattern line = $line\n" if $options{verbose};
 
@@ -53,6 +58,8 @@ for my $line (split("\n", $patterns_config)) {
   my %pattern_attributes;
   $owner = '' if $owner eq '-';
   $group = '' if $group eq '-';
+  $dperm = zero_pad_octal_string($dperm);
+  $fperm = zero_pad_octal_string($fperm);
   @pattern_attributes{qw/owner group dperm fperm/}
     = ($owner, $group, $dperm, $fperm);
 
@@ -69,14 +76,14 @@ sub get_id {
   return -1;
 }
 
-sub oct_str_to_perm {
-  # safely convert octal string mode to numeric for use with chmod
-  my ($str) = @_;
-  if (substr($str, 0, 1) ne '0') {
-    $str = '0' . $str;
-  }
-  return oct($str);
-}
+#sub zero_pad_octal_string {
+#  # safely convert octal string mode to numeric for use with chmod
+#  my ($str) = @_;
+#  if (substr($str, 0, 1) ne '0') {
+#    $str = '0' . $str;
+#  }
+#  return oct($str);
+#}
 
 print "starting descent at $options{dir}\n" if $options{verbose};
 
@@ -99,13 +106,15 @@ find({no_chdir => 1, wanted => sub {
       #use Data::Dumper;
       #print Dumper $pa;
 
+      my $sb = stat($f);
 
       if ($options{no_run}) {
         # TODO - improve output
         my $new_owner = $pa->{owner} . ':' . $pa->{group};
         print "chown $new_owner $f\n" if ($new_owner ne ':');
-        my $uid = get_id($pa->{owner}, \&CORE::getpwnam);
-        my $gid = get_id($pa->{group}, \&CORE::getgrnam);
+        my $uid = get_id($pa->{owner}, \&POSIX::getpwnam);
+        my $gid = get_id($pa->{group}, \&POSIX::getgrnam);
+
         print "chown $uid, $gid, $f\n";
       } else {
         # TODO - really run it
@@ -113,29 +122,45 @@ find({no_chdir => 1, wanted => sub {
         # TODO - check status
       }
 
-      #print $pa->{dperm} . "\n";
-      #print "$f\n";
-      #use Cwd;
-      #print 'cwd = ' . getcwd() . "\n";
-      #print "dir\n" if (-d "$f");
+      #printf "mode is " . ($sb->mode & 07777) . "\n";
+
+      my $current_mode = sprintf('%04o', $sb->mode & 07777);
+      printf "current mode is $current_mode\n" if $options{verbose};
+      #my $fperm = oct_str_to_perm($pa->{fperm});
+      #my $dperm = oct_str_to_perm($pa->{dperm});
+      my $fperm = $pa->{fperm};
+      my $dperm = $pa->{dperm};
+
+      # if dperm specified and is a dir
       if ($pa->{dperm} ne '-' && -d $f) {
-        if ($options{no_run}) {
-          print 'chmod ' . oct_str_to_perm($pa->{dperm}) .
-                '(' . $pa->{dperm} . "), $f # DIR\n";
+        print "target DIR mode is $dperm\n";
+
+        # if new perm is different
+        if ($current_mode != $dperm) {
+          print "chmod $dperm (" . $pa->{dperm} . "), $f # DIR\n";
+          if ($options{no_run}) {
+            # TODO - uncomment
+            print 'run: chmod ' . oct_str_to_perm($pa->{dperm}) . ", $f\n";
+            # TODO - check status
+          }
         } else {
-          # TODO - uncomment
-          #chmod oct_str_to_perm($pa->{dperm}), $f;
-          # TODO - check status
+          print "no changed needed\n" if $options{verbose};
         }
       }
 
+      # if fperm specified and is a file
       if ($pa->{fperm} ne '-' && -f $f) {
-        if ($options{no_run}) {
-          print 'chmod ' . oct_str_to_perm($pa->{fperm}) .
-                '(' . $pa->{fperm} . "), $f # FILE\n";
+        print "target FILE mode is $fperm\n";
+        if ($current_mode != $fperm) {
+          print "chmod $fperm (" . $pa->{fperm} . "), $f # FILE\n";
+          if ($options{no_run}) {
+            # TODO - uncomment
+            print 'run: chmod ' . oct_str_to_perm($pa->{fperm}) . ", $f\n";
+            # TODO - check status
+          }
         } else {
-          # TODO - uncomment
           #chmod oct_str_to_perm($pa->{fperm}), $f;
+          print "no changed needed\n" if $options{verbose};
         }
       }
 
@@ -150,16 +175,17 @@ __END__
 
 recursively set permissions and ownerships according to pattern rules
 
-=head1 SYNOPSIS
+=head1 OPTIONS
 
 jperms.pl [options] [dir ...]
 
  Options:
-   -help|-h|-?    help
-   -man           full documentation
-   -dir           directory to descend
-   -verbose       extra debug
-   -no_run        don't run any commands
+   -help|-h|-?     help
+   -man            full documentation
+   -dir            directory to descend
+   -file           patterns file
+   -verbose        extra debug
+   -no_run         don't run any commands
 
 =head1 DESCRIPTION
 
@@ -182,7 +208,7 @@ found during the tree descent.
 - Patterns are automatically surrounded by ^ and $ when matching, meaning
 that it must match the whole of the current path (not just part of it).
 
-- File paths include the top level path specified on the command line.
+- file paths include the top level path specified on the command line.
 
 - The path of each file or directory found during the descent is compared
 against each pattern in turn (top to bottom) until a match is found.  Pattern
@@ -202,3 +228,23 @@ applied to directories, and file_perms are only applied to files
 
 Lines starting with a '#' (optionally preceeded by whitespace)
 are treated as comments and ignored.
+
+=head1 EXAMPLE
+
+Example pattern file:
+
+# pattern                      owner   group  dperm  fperm
+
+\./test/data.*/proj/cards      appuser grp2    770   -
+\./test/data.*/proj/r/r_import appuser grp2    770   -
+\./test/data.*/proj/outbound   appuser adm    2770   -
+\./test/data.*/proj/.*/inbox   appuser grp2    770   -
+\./test/data.*/proj/[^/]+      appuser grp2    750   -
+\./test/data[^/]*/proj         appuser adm     750   -
+\./test/data.*/tws/logs        appuser grp2    770   -
+\./test/data.*/tws             appuser grp2    750   -
+\./test/data.*/logs            appuser grp2    770   -
+\./test/data.*/scripts         root    adm     750   -
+\./test/data.*                 appuser grp2    770   660
+\./test/.*                     -       -       -     -
+\./test                        appuser appgrp  775   -
